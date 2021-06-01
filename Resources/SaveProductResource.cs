57f33befaa03b3da8733df61b90da78f55834bd3
Zotev <a.zotev@yahoo.com>
2021-06-01 12:31:09 +0300

    small changes

--- Resources/SaveProductResource.cs
+++ Resources/SaveProductResource.cs
@@ -9,10 +9,8 @@ namespace Supermarket.API.Resources
         [MaxLength(30)]
         public string Name { get; set; }
 
-        [Required]
         public int QuantityInStock { get; set; }
 
-        [Required]
         public decimal UnitPrice { get; set; }
     }
 }
